f2d357113b025acd8bbdeeedf16ebf474b1cb51c
Alexey Dudin <alex-dude@yandex.ru>
2025-02-13 12:54:09 +0300

    Add initial debugger support

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,16 +1,113 @@
 --
 -- nvim-dap is a Debug Adapter Protocol client implementation for Neovim
 --
+
+
+local function keymaps()
+    local vim = vim
+    local dap = require('dap')
+
+    vim.keymap.set('n', '<F4>', function() dap.run_to_cursor() end)
+    vim.keymap.set('n', '<F5>', function() dap.continue() end)
+    vim.keymap.set('n', '<F8>', function() dap.step_over() end)
+    vim.keymap.set('n', '<F7>', function() dap.step_into() end)
+    vim.keymap.set('n', '<F11>', function() dap.step_out() end)
+    vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
+    vim.keymap.set('n', '<C-F2>', function() dap.terminate() end)
+    vim.keymap.set('n', '<F10>', function() dap.focus_frame() end)
+
+    vim.keymap.set('n', '<Leader>lp', function()
+        dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+    end)
+
+    vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
+
+    vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
+
+    vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+        require('dap.ui.widgets').hover()
+    end)
+
+    vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+        require('dap.ui.widgets').preview()
+    end)
+
+    vim.keymap.set('n', '<Leader>df', function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.frames)
+    end)
+
+    vim.keymap.set('n', '<Leader>ds', function()
+        local widgets = require('dap.ui.widgets')
+        widgets.centered_float(widgets.scopes)
+    end)
+end
+
 return {
     {
         'mfussenegger/nvim-dap',
+        dependencies = {
+            'ray-x/guihua.lua',
+        },
+
+        config=function()
+            local dap = require("dap")
+
+            dap.adapters.gdb = {
+                type = "executable",
+                command = "/home/dude/bin/yagdb",
+                args = {
+                    "--interpreter=dap",
+                    "--quiet",
+                    -- "--eval-command",
+                    -- "set print pretty on",
+                },
+            }
+
+            dap.configurations.cpp = {
+              {
+                type = 'gdb';
+                request = 'launch';
+                name = "Launch file";
+                program = function()
+                    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end;
+              },
+            }
+
+            keymaps()
+        end,
     },
 
     {
         'rcarriga/nvim-dap-ui',
+
+        requires = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+
+
+        config = function()
+            local dapui = require("dapui")
+            dapui.setup()
+
+            local dap = require("dap")
+
+            dap.listeners.before.attach.dapui_config = function() dapui.open() end
+            dap.listeners.before.launch.dapui_config = function() dapui.open() end
+            dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+            dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+        end,
+
     },
 
     {
         'theHamsta/nvim-dap-virtual-text',
+
+        config = function()
+            require("nvim-dap-virtual-text").setup()
+        end,
     }
 }
+
